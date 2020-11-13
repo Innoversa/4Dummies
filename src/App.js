e783bd1590bbb001dbc26fae45c0b0ee3d05545d
alreadychosen <32781166+alreadychosen@users.noreply.github.com>
2020-11-12 22:17:15 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,28 +2,96 @@ import logo from './logo.svg';
 import './App.css';
 import Food from './Food.js';
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
 
 
 
 
 
 function App() {
-
+  const [cards, addCards] = React.useState([]);
+  const [name, setName] = React.useState('Banana');
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2020-11-13T21:11:54'));
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+  function deleteCard(ind){
+    console.log("before: ");
+    var temp = cards;
+    console.log(cards);
+    for( var i= 0; i < cards.length; i++){
+      if(cards[i].key === ind){
+        addCards(temp.splice(i,1));
+      }
+      break;
+    }
+   addCards(temp);
+  }
+  function addCard(food){
+    var tempName = name;
+    var tempDate = selectedDate;
+    var tempArray = cards;
+    var ind = cards.length;
+    addCards([... cards, <Food text ={tempName}   key = {ind} exp ={tempDate.toString().substr(0,16)} buttonClick={()=> deleteCard(ind)}/> ]);
+    console.log("in addCard function");
+    console.log(cards);
+    console.log(ind);
+ 
+  }
   return (
     
     <div className="App">
+      
+      <div className="foodAdder">
+      <h3>Enter food:</h3>
+      <TextField id="standard-name" label="Food:" value={name} onChange={handleChange} />
+      <div className="date">
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      
+    
+      
+        <KeyboardDatePicker
+          margin="normal"
+          id="date-picker-dialog"
+          label="Expiration date:"
+          format="MM/dd/yyyy"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+       
+   
+    </MuiPickersUtilsProvider>
+    </div>
+    
+    <Button variant="outlined" color="primary" onClick = {addCard}>
+      Add food
+    </Button>
+      </div>
+      <br />
+      <br />
       <div className="Cards">
-        <Food text ="Food name" />
-        <Food text ="Food name" />
-        <Food text ="Food name" />
-        <Food text ="Food name" />
-        <Food text ="Food name" />
-        <Food text ="Food name" />
-        <Food text ="Food name" />
-        <Food text ="Food name" />
+      {cards.map(function(item) {
+      return item;
+    })}
+      
       </div>
       <div className="itemAdder"></div>
 
--- src/Food.js
+++ src/Food.js
@@ -9,29 +9,60 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import "./Food.css";
 function Food(props){
+    
+    const [img, setImg] = React.useState(null);
+
+    React.useEffect(async () => {
+        const response = await fetch('https://api.imgur.com/3/gallery/search/top?q=' + props.text + ' ext: png OR jpg', 
+            {
+                method: 'get',
+                headers: {
+                    //"Access-Control-Allow-Headers": "Authorization",
+                    "Authorization": "Client-ID e4f15a9a27fb199" //This header may be wrong
+                }
+            });
+        //const response = await fetch('https://api.randomuser.me/')
+        if (response.status !== 200) {
+            console.log('Error performing imgur api call. Status Code ' + response.status);
+        }
+        const json = await response.json();
+        //const item = null;
+        console.log(json);
+        for (var i = 0; i < json.data.length; i++) {
+            if (json.data[i].nsfw || json.data[i].is_album) {
+                continue;
+            }
+            const item = json.data[i];
+            console.log(item);
+            setImg(item);
+            break;
+        }
+        //const item = json.results[0];
+        //setImg(item);
+    }, [])
 
     return (
             
             <Card className="foodCard">
             <CardActionArea>
-            <CardMedia
+           { img && <CardMedia
                 component="img"
-                alt="Contemplative Reptile"
+                alt={props.text}
                 height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
+                image={img.link}
                 title="Contemplative Reptile"
-            />
+            />}
             <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
                 {props.text}
                 </Typography>
                 <Typography variant="body2" color="textSecondary" component="p">
-                
+                Expires on {props.exp}
                 </Typography>
             </CardContent>
             </CardActionArea>
             <CardActions>
-            <Button size="small" color="primary">
+            <Button size="small" color="primary" onClick = {props.buttonClick}>
                 Delete item
             </Button>
             
